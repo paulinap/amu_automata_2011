4e91ee61bf39f75170a374fca8c17963522383d2
Paulina Pęsik <s369964@pc15l.labs.wmi.amu.edu.pl>
2011-12-08 11:43:51 +0100

    #278 poprawka 1

--- daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomatonUtilities.java
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomatonUtilities.java
@@ -7,33 +7,34 @@ import java.util.Set;
  * 
  */
 public class AutomatonUtilities {
-    
+
+    private AutomatonUtilities() {
+    }
+
     static Set<Character> getAlphabet(AutomatonSpecification automaton, Set<Character> superset) {
-        
+
         Set<Character> alphabet = new HashSet<Character>();
-        
+
         for (State s : automaton.allStates()) {
             for (OutgoingTransition ot : automaton.allOutgoingTransitions(s)) {
                 for (Character c : superset) {
 
-			boolean isAlreadyIn = false;
-			for(Character ch : alphabet)
-                            if(ch == c)	{
-                                isAlreadyIn = true;
-                                break;
-                            }
+                    boolean isAlreadyIn = false;
+                    for (Character ch : alphabet) {
+                        if (ch.equals(c)) {
+                            isAlreadyIn = true;
+                            break;
+                        }
+                    }
 
-			if(!isAlreadyIn)
-                            if (ot.getTransitionLabel().canAcceptCharacter(c)) {
-				alphabet.add(c);
-				continue;
-                            }
-		}
+                    if (!isAlreadyIn && ot.getTransitionLabel().canAcceptCharacter(c)) {
+                        alphabet.add(c);
+                    }
+                }
             }
         }
-        
+
         return alphabet;
-                
+
     }
-    
 }
